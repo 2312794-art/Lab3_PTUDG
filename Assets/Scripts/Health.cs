6fbacfc94285e3f0eaa8049be08cd187c9ea5ffa
Ahmed Nassef <popcar@protonmail.com>
2025-09-26 17:58:33 +0300

    Screen Shake

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,6 +5,14 @@ public class Health : MonoBehaviour
     [SerializeField] int health = 50;
     [SerializeField] ParticleSystem hitParticles;
 
+    [SerializeField] bool applyCameraShake;
+    CameraShake cameraShake;
+
+    void Start()
+    {
+        cameraShake = Camera.main.GetComponent<CameraShake>();
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
@@ -14,6 +22,11 @@ public class Health : MonoBehaviour
             TakeDamage(damageDealer.GetDamage());
             PlayHitParticles();
             damageDealer.Hit();
+
+            if (applyCameraShake)
+            {
+                cameraShake.Play();
+            }
         }
     }
 
